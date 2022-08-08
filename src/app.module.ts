1641263926c1a9ee32071be3adcd2b9f42cefd98
NJAO <1157515428@qq.com>
2022-08-09 01:49:26 +0800

    middleware try

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,15 @@
-import { Module } from '@nestjs/common';
+import { Module, MiddlewareConsumer, RequestMethod } from '@nestjs/common';
+import { LoggerMiddleware } from './common/middleware/logger.middleware';
 import { IndexModule } from './modules/wechat/index/index.module';
 
 @Module({
   imports: [IndexModule],
 })
-export class AppModule {}
+export class AppModule {
+  configure(consumer: MiddlewareConsumer) {
+    consumer
+      .apply(LoggerMiddleware)
+      .exclude({ path: 'wechat/index', method: RequestMethod.POST }) // 中间件不处理改路径的post请求
+      .forRoutes('wechat'); // 中间件将wechat作为根节点
+  }
+}
